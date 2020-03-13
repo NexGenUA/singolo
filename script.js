60700ec536b15fd8b1cded546054e0d9adfd8464
NexGenUA <mr.rinus@gmail.com>
2020-03-13 23:24:42 +0200

    feat: slider complete

--- script.js
+++ script.js
@@ -1,17 +1,88 @@
-const menu = document.querySelector('#menu-list');
+class ActiveLink {
+  constructor() {
+    this.menu  = document.querySelector('#menu-list');
+    this.menu.addEventListener('click', this.setActiveLink)
+  }
 
-const setActiveLink = e => {
-  
-  const link = e.target;
-
-  if (link.tagName !== 'A') return; 
-  
-  const liList = link.closest('ul').querySelectorAll('li');
-  
-  liList.forEach(el => el.classList.remove('active'))
-
-  link.parentNode.classList.add('active')
-
+  setActiveLink(e) {
+    const link = e.target;
+    if (link.tagName !== 'A') return; 
+    const liList = link.closest('ul').querySelectorAll('li');
+    liList.forEach(el => el.classList.remove('active'))
+    link.parentNode.classList.add('active')
+  }
 }
 
-menu.addEventListener('click', setActiveLink);
+class Slider {
+  constructor() {
+    this.sliders = document.querySelectorAll('.img-slider-container');
+    this.screens = document.querySelectorAll('.screen-on');
+    this.prev = document.querySelector('#prev-button');
+    this.next = document.querySelector('#next-button');
+    this.prev.onclick = this.next.onclick = this.changeSlider;
+    this.screens.forEach(el => el.onclick = this.onScreen);
+    this.isMove = false;
+  }
+
+  changeSlider = e => {
+
+    if (this.isMove) return;
+    
+    const sliders = Array.from(this.sliders);
+    const btn = e.target.closest('button');
+
+    if (btn.tagName !== 'BUTTON') return;
+    
+    const direction = btn.classList.contains('prev');
+
+    this.move(sliders, direction);
+    
+  }
+
+  move(sliders, prev) {
+    
+    this.isMove = true;
+
+    const width = document.querySelector('.slider-wrapper').clientWidth;
+    const shift = prev ? -1 : 1;
+    const currentSlide = sliders.find(el => el.hasAttribute('current-slide'));
+    const currentIdx = sliders.indexOf(currentSlide);
+    const nextIdx = this.getIdx(currentIdx, shift, sliders.length - 1);
+    const nextSlide = sliders[nextIdx];
+
+    [currentSlide, nextSlide].map((slide, i) => {
+      slide.style.display = 'flex';
+      slide.style.transition = 'all .5s linear';
+      
+      if (!i) {
+        slide.style.marginLeft = `${prev ? '-' : ''}${width}px`;        
+      } else {
+        slide.style.marginLeft = `${!prev ? '-' : ''}${width}px`;
+        setTimeout(() => {
+          slide.style.marginLeft = '';
+        }, 50)
+      }
+
+      setTimeout(() => {
+        slide.style.transition = '';
+        if (!i) slide.style.display = 'none';
+        this.isMove = false;
+      }, 500)
+    })
+    currentSlide.removeAttribute('current-slide');
+    nextSlide.setAttribute('current-slide', '');
+  }
+
+  onScreen({ target }) {
+    console.log('hi')
+    target.classList.toggle('screen-on');
+  }
+
+  getIdx(cur, shift, len) {
+    const result = cur + shift;
+    return result < 0 ? len : result > len ? 0 : result;
+  }
+}
+
+new ActiveLink();
+new Slider();
